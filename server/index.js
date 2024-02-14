c84b1127e1f30727273dc68651df5bc20d5e6ed6
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-15 00:24:00 +0530

    create schema for User and Residency

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.listen(PORT, (err, res) => {
     console.log(`Server listening on ${PORT}`);
