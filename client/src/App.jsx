8124625a0773e5219a7f6f98949116532cd8d35b
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-16 15:58:23 +0530

    properties page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,27 @@ import Website from "./pages/Website";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Properties from "./pages/Properties/Properties";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ReactQueryDevtools } from "react-query/devtools";
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route element={<Layout />} >
-            <Route path="/" element={<Website />} />
-            <Route path="/properties" element={<Properties />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />} >
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <ToastContainer />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
