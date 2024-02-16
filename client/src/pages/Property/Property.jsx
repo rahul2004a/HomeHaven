6084e61aea341771b65f222e64d586ff74c1b0fc
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-17 00:30:11 +0530

    property page

---
+++ client/src/pages/Property/Property.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import './Property.css';
+import { useLocation } from 'react-router-dom';
+import { useQuery } from "react-query";
+import { getProperty } from '../../utils/api';
+import { PuffLoader } from "react-spinners";
+import { AiFillHeart } from "react-icons/ai";
+import { FaShower } from "react-icons/fa";
+import { AiTwotoneCar } from "react-icons/ai";
+import { MdLocationPin, MdMeetingRoom } from "react-icons/md";
+
+const Property = ({ card }) => {
+    const { pathname } = useLocation();
+    const id = pathname.split("/").slice(-1)[0];
+    const { data, isLoading, isError } = useQuery(["resd", id], () =>
+        getProperty(id)
+    );
+    console.log(data);
+    if (isLoading) {
+        return (
+            <div className="wrapper">
+                <div className="flexCenter paddings">
+                    <PuffLoader />
+                </div>
+            </div>
+        );
+    }
+
+    if (isError) {
+        return (
+            <div className="wrapper">
+                <div className="flexCenter paddings">
+                    <span>Error while fetching the property details</span>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className='wrapper'>
+            <div className="flexColStart paddings innerWidth property-container">
+                <div className="like">
+                    <AiFillHeart size={24} color="white" />
+                </div>
+                <img src={data?.image} alt="home image" />
+                <div className='flexCenter property-details'>
+                    <div className="flexColStart left">
+                        <div className="flexStart head">
+                            <span className='primaryText'>{data?.title}</span>
+                            <span className='orangeText' style={{ font: '1.5rem' }}>₹{data?.price}</span>
+                        </div>
+                        <div className="flexStart facilities">
+                            <div className="flexStart facility">
+                                <FaShower size={20} color="#1F3E72" />
+                                <span>{data?.facilities?.bathrooms} Bathrooms</span>
+                            </div>
+                            <div className="flexStart facility">
+                                <AiTwotoneCar size={20} color="#1F3E72" />
+                                <span>{data?.facilities?.parking} Parking</span>
+                            </div>
+
+                            <div className="flexStart facility">
+                                <MdMeetingRoom size={20} color="#1F3E72" />
+                                <span>{data?.facilities?.bedrooms} Room/s</span>
+                            </div>
+                        </div>
+                        <span className="secondaryText" style={{ textAlign: "justify" }}>
+                            {data?.description}
+                        </span>
+                        <div className="flexStart" style={{ gap: "1rem" }}>
+                            <MdLocationPin size={25} />
+                            <span className="secondaryText">
+                                {data?.address}{" "}
+                                {data?.city}{" "}
+                                {data?.country}
+                            </span>
+                        </div>
+                        <button className='button'>Book Your Visit</button>
+                    </div>
+                    <div className="right">
+                        this is right
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Property
