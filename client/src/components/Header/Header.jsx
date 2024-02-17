339a3201396b1004a037a22a89d33309127d3749
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-02-17 09:31:17 +0530

    add auth0  for user login and authentication

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,10 +5,13 @@ import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import Profilemenu from "../ProfileMenu/Profilemenu";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
 
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
@@ -32,10 +35,15 @@ const Header = () => {
 
             <NavLink to="/properties">Properties</NavLink>
             <a href="mailto:kumar.rahul.2017354@gmail.com">Contact</a>
-
-            <button className="button">
-              Login
-            </button>
+            {
+              !isAuthenticated ?
+                <button className="button" onClick={loginWithRedirect}>
+                  Login
+                </button> :
+                <div>
+                  <Profilemenu user={user} logout={logout} />
+                </div>
+            }
           </div>
         </OutsideClickHandler>
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { Auth0Provider } from "@auth0/auth0-react";
+import { MantineProvider } from '@mantine/core';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-84lq4qdikixm2nuq.us.auth0.com"
+      clientId="TlU13n3aOtluDxKexUCKOZ6kHdee3WaG"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173/"
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
+    >
+
+
+      <App />
+
+    </Auth0Provider>
   </React.StrictMode>
 );
